9cf8a269ddad8303eb4a2b13da0dbb187f97fd7e
dev-loic <loic.saillant.ad@gmail.com>
2018-10-28 20:37:20 +0400

    Ajout de PremiersProgrammes

---
+++ TDs/src/PremiersProgrammes/Bonjour.java
@@ -0,0 +1,10 @@
+package PremiersProgrammes;
+
+public class Bonjour {
+
+	public static void main(String [] args) { 
+		// Affiche le message ”Bonjour !” 
+		System.out.println(" Bonjour !");
+	}
+	
+}
