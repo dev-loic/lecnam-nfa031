ab283ce782d2adf7ceae562a144e02fd711df9fc
dev-loic <loic.saillant.ad@gmail.com>
2018-11-11 09:45:50 +0200

    Correction unsigned int

--- TDs/src/cours04/TestUnsignedInt.java
+++ TDs/src/cours04/TestUnsignedInt.java
@@ -13,7 +13,7 @@ public class TestUnsignedInt {
 		System.out.println(" Utilisation des méthodes unsigned");
 		System.out.println(" --> i(attendu) = 2147483747 \n --> i(reel)    = " + Integer.toUnsignedString(i));
 		System.out.println(" Si on considère i comme un int");
-		System.out.println(" --> i(attendu) = -2147483449 \n --> i(reel)    = " + i + "\n");
+		System.out.println(" --> i(attendu) = -2147483549 \n --> i(reel)    = " + i + "\n");
 		
 		i = i + 100; // i doit maintenant être égal à 2147483847
 		System.out.println(" Utilisation des méthodes unsigned");
