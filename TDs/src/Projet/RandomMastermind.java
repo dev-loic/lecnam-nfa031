391d8f7fcfb2b5dc7ab4168dadf6a162b6ca6b70
Loïc Saillant <loic.saillant@fabernovel.com>
2020-01-07 21:52:35 +0100

    Improve randomMastermind

--- TDs/src/Projet/RandomMastermind.java
+++ TDs/src/Projet/RandomMastermind.java
@@ -2,7 +2,6 @@ package Projet;
 
 public class RandomMastermind {
 
-	// Tableau de reference
 	static String [] TAB_REF_COLORS = {
 			"rouge",
 			"jaune",
@@ -13,52 +12,38 @@ public class RandomMastermind {
 			"violet",
 			"fuchsia"
 	};
+	
+	static int NB_COLORS = 4;
 
 	public static void main(String[] args) {
 		// Pour utiliser generateRandomCombination, il suffit de recupérer le tableau de couleurs généré. 
 		String [] combinaisonSecrete = generateRandomCombination();
-
-		for(int i=0;i<4;i++)
+		for(int i=0;i<4;i++) {
 			System.out.println(combinaisonSecrete[i]);
+		}
 	}
 
 	// Generate random combination of 4 colors 
 	static String [] generateRandomCombination() {
-		String [] combination = new String[4];
-
-		for (int i=0;i<4;i++) {
-			int index = 0;
-			String randomColor = "";
-			boolean alreadyIn = true;
-
-			do 
-			{
-				index = (int)(7*Math.random());
-				randomColor = TAB_REF_COLORS[index];
-				if(!isIn(combination,randomColor)) alreadyIn = false;
-			} while (alreadyIn);
-
-			combination[i] = randomColor;
+		String [] combination = new String[NB_COLORS];
+		int currentPosition = 0;
+		while(currentPosition!=NB_COLORS) {
+			int indexRandom = (int)(Math.random()*TAB_REF_COLORS.length);
+			String color = TAB_REF_COLORS[indexRandom];
+			if(!isIn(color, combination)) {
+				combination[currentPosition] = color;
+				currentPosition++;
+			}
 		}
-
 		return combination;
 	}
 
-	static boolean isIn(String [] iTab, String iStringToFind)
-	{
-		boolean output = false;
+	static boolean isIn(String iStringToFind, String [] iTab) {
 		int size = iTab.length;
-
-		for(int i=0;i<size;i++)
-		{
-			if(iStringToFind.equals(iTab[i]))
-			{
-				output=true;
-				break;
-			}
+		for(int i=0;i<size;i++) {
+			if(iStringToFind.equals(iTab[i])) return true;
 		}
-
-		return output;
+		return false;
 	}
 
 }
