6ded2a0b4cd1458b4fd616a490195bfb22f1ab0d
Loïc Saillant <loic.saillant@fabernovel.com>
2020-03-19 17:00:53 +0100

    Correction recuperation au clavier

--- TDs/src/PremiersProgrammes/RecupererDonneesAuClavier.java
+++ TDs/src/PremiersProgrammes/RecupererDonneesAuClavier.java
@@ -2,8 +2,16 @@ package PremiersProgrammes;
 
 import java.util.Scanner;
 
-public class RecuperDonneesAuClavier {
-
+public class RecupererDonneesAuClavier {
+	
+	/*
+	 * CONSEILS D'UTILISATION
+	 * Certaines lignes de code sont volontaire commentées (présence des // devant la ligne)
+	 * En effet, l'usage des méthodes nextInt et nextDouble avec nextLine ne fait pas bon ménage.
+	 * L'idéal est de TOUJOURS utiliser la forme Integer.parseInt(sc.nextLine()); pour récupérer un entier
+	 * et Double.parseDouble(sc.nextLine()); pour recupérer un nombre à virgule
+	 */
+	
 	@SuppressWarnings("unused") // uniquement parce que j'initialise des variables que je n'utilise pas (ne pas mettre dans vos programmes)
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in); // creation du scanner
@@ -11,15 +19,15 @@ public class RecuperDonneesAuClavier {
 		// 1 - Récupération d'un entier (int)
 		// Les 2 méthodes ont le même résultat, seulement la deuxième permet de prévenir d'erreur
 		// Donc je vous la conseille. Si dans un premier temps elle vous paraît compliquée, utilisez la 1ère
-		System.out.println("Saisissez un entier : "); // cette ligne est uniquement pour indiquée à l'utilisateur de saisir une info
-		int entierDeMonUtilisateur = sc.nextInt(); 
-		System.out.println("Saisissez un autre entier : "); // idem
+		//System.out.println("Saisissez un entier : "); // cette ligne est uniquement pour indiquée à l'utilisateur de saisir une info
+		//int entierDeMonUtilisateur = sc.nextInt(); 
+		System.out.println("Saisissez un entier : "); // idem
 		int autreEntierDeMonUtilisateur = Integer.parseInt(sc.nextLine());
 		
 		// 2 - Récupération d'un nombre à virgule (double)
+		//System.out.println("Saisissez un nombre : ");
+		//double nombreDeMonUtilisateur = sc.nextDouble();
 		System.out.println("Saisissez un nombre : ");
-		double nombreDeMonUtilisateur = sc.nextDouble();
-		System.out.println("Saisissez un autre nombre : ");
 		double autreNombreDeMonUtilisateur = Double.parseDouble(sc.nextLine());
 		
 		// 3 - Récupération d'un chaine de caractère (String)
